96bbfd49ad2af25bc4f69ee1006892403a3f4d44
a10213470 <10213470@qq.com>
2020-11-08 20:04:47 +0800

    test

--- simplelayout.py
+++ simplelayout.py
@@ -1,9 +1,22 @@
-# import argparse
+import argparse
 
 
 def main():
-    raise NotImplementedError  # TODO: 删除该行，并在下方实现代码
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("board_grid", help="display a square of a given number",
+                    type=int)
+    parser.add_argument("unit_grid", help="display a square of a given number",
+                    type=int)
+    parser.add_argument("unit_n", help="display a square of a given number",
+                    type=int)
+    parser.add_argument("unit_n", help="display a square of a given number",
+                    type=list)
+    parser.add_argument("outdir", help="display a square of a given number",
+                    type=str)
+    parser.add_argument("file_name", help="display a square of a given number",
+                    type=str)
+    args = parser.parse_args()
+    print(args.square**2)
 
 if __name__ == "__main__":
     main()
